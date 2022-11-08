e35da8b6bc0d59a5e61fb9c4021b1217f135bf80
ayodvr <ayodejiadekunle@gmail.com>
2022-11-08 17:33:44 +0100

    yexma

--- resources/views/facility/create.blade.php
+++ resources/views/facility/create.blade.php
@@ -8,7 +8,7 @@
           <div class="col-12 col-md-12 col-lg-12">
             <div class="card">
               <div class="card-header">
-                <h4 class="text-center">Daily Report Form</h4>
+                <h4 class="text-center">Report Form</h4>
               </div>
               <div class="card-body">
                 <form method="POST" action="{{ route('facility-report.store') }}" id="form">
